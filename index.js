08dbdc35847ce4aa37fa8db863df7e3e22435a82
Milton Ngeno <ngenomilton10@gmail.com>
2025-07-02 12:48:26 +0300

    adds creating and inserting Dom nodes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main =document.querySelector('#main')
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Logic Forge is the champion"
